8f024bcacf1f1f3d218799b45f7f606321415296
Augusto Wink <164242873+gutowink@users.noreply.github.com>
2024-04-22 11:44:37 -0300

    att03

--- 2024-04-22/horoscopo.py
+++ 2024-04-22/horoscopo.py
@@ -1,6 +1,6 @@
 signos = ('Python','Fortran','Rust','Go','Java','JavaScript','C','PHP','Haskell','Perl','Swift','Kotlin')
 meses = ('Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro')
-mes = int(input('Digite o mês do seu nascimento em numeral, de 1 para janeiro até 12 para dezembro: '))
+mes = int(input('Digite o mês do seu nascimento em número inteiro, de 1 para janeiro até 12 para dezembro: '))
 if mes <= 0 or mes >= 13:
         print('Você não é um programador.')
 else:
